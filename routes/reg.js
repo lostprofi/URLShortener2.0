ebeedded8e060c608fe056f853c66dc792c93371
lostprofi <o-lazarchik@mail.ru>
2020-05-23 20:02:35 +0300

    Finished developing registration server.

---
+++ routes/reg.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const User = require('../dataBase/models');
+
+const router = express.Router();
+
+router.post('/reg', [
+  check('username', 'Please enter your name').not().isEmpty(),
+  check('email', 'Please enter correct e-mail adress').isEmail(),
+  check('password', 'Please enter password with 6 or more symbols').isLength({ min: 6 }),
+  check('passwordConfirm').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password confirmation is incorrect');
+    } else {
+      return true;
+    }
+  })
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(500).json({ errors: errors.array() });
+  }
+
+  const {
+    username, email, password, links,
+  } = req.body;
+
+  try {
+    const isUserExist = await User.findOne({ email });
+
+    if (isUserExist) {
+      return res.status(500).json({ errors: [{ msg: 'User is already exist' }] });
+    }
+
+    const hashPassword = await bcrypt.hash(`${password}`, 10);
+
+    const user = new User({
+      username,
+      email,
+      _password: hashPassword,
+      links,
+    });
+
+    await user.save();
+    return res.status(201).send('User successfully registered ');
+  } catch (err) {
+    return res.status(500).json({ errors: [{ msg: 'Server error' }] });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const config = require('config');
+const connectDB = require('./dataBase/connectDB');
+
+const app = express();
+const PORT = config.get('serverPort');
+
+connectDB();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', ['Content-Type', 'x-auth-token']);
+  next();
+});
+
+app.use('/', require('./routes/reg'));
+
+app.listen(PORT, () => {
+  console.log('Server started on port 5000');
+});
